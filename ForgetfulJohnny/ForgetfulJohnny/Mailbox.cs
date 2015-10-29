6361275beb8cf15338163f213fa79cd41e159d3e
lohkii69 <jmckay2@hotmail.com>
2015-10-29 15:02:10 -0600

    Initial classes added

---
+++ ForgetfulJohnny/ForgetfulJohnny/Mailbox.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ForgetfulJohnny
+{
+    class Mailbox
+    {
+        public static string Pass(string mail)
+        {
+            if ("" == mail) return null;
+            mail = mail.Substring(0, mail.IndexOf("@"));
+            var password = mail.ToCharArray();
+            var numbers = new List<char>();
+            var letters = new List<char>();
+            foreach (var character in password)
+            {
+                if (char.IsDigit(character))
+                {
+                    numbers.Add(character);
+                }
+                else
+                {
+                    letters.Add(char.IsUpper(character) ? char.ToLower(character) : char.ToUpper(character));
+                }
+            }
+
+            var pass = string.Join("", numbers) + string.Join("", letters);
+
+            return pass;
+        }
+
+
+        static void Main(string[] args)
+        {
+            Pass("exampl1e3@gmail.com");
+
+        }
+    }
+}
---
+++ ForgetfulJohnny/ForgetfulJohnny/TestClass.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+
+
+namespace ForgetfulJohnny
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void TestExampleOne()
+        {
+            Assert.AreEqual("VASUAussr", Mailbox.Pass("vasuaUSSR@yandex.ru"));
+        }
+
+        [Test]
+        public void TestExampleTwo()
+        {
+            Assert.AreEqual("111hIrOdOt", Mailbox.Pass("HiRoDoT111@gmail.com"));
+        }
+
+        [Test]
+        public void TestExampleThree()
+        {
+            Assert.AreEqual("1323356ExAmPl", Mailbox.Pass("1eXaM32pL3356@gmail.com"));
+        }
+
+        [Test]
+        public void EmptyMail()
+        {
+            Assert.AreEqual(null, Mailbox.Pass(""));
+        }
+    }
+}
