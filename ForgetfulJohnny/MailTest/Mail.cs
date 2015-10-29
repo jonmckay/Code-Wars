bcd77a69cec4b20f3c7cb8be52b00d94a2124d36
lohkii69 <jmckay2@hotmail.com>
2015-10-29 14:58:04 -0600

    Initial commit.

---
+++ ForgetfulJohnny/MailTest/Mail.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MailTest
+{
+    [TestClass]
+    public class Mail
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Pass()
+        }
+
+        [TestMethod]
+        public void testExampleOne()
+        {
+            Assert.AreEqual("VASUAussr", Mailbox.Pass("vasuaUSSR@yandex.ru"));
+        }
+
+        [TestMethod]
+        public void testExampleTwo()
+        {
+            Assert.AreEqual("111hIrOdOt", Mailbox.Pass("HiRoDoT111@gmail.com"));
+        }
+    
+
+    }
+}
