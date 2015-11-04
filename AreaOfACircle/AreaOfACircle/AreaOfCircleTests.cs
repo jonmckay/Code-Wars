6d9a0e2b95ca7e8440a4a15904e0b4e1d71fb484
lohkii69 <jmckay2@hotmail.com>
2015-11-04 15:07:04 -0700

    Initial commit calculating the area of a circle given a radius

---
+++ AreaOfACircle/AreaOfACircle/AreaOfCircleTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaOfACircle
+{
+    using System;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class AreaOfCircleTests
+    {
+        [Test]
+        public void ShouldFailNegativeNumber()
+        {
+            Assert.Throws<ArgumentException>(delegate { Program.CalculateAreaOfCircle("-123"); });
+            //Assert.Throws(typeof(ArgumentException), Kata.CalculateAreaOfCircle("-123"));
+        }
+
+        [Test]
+        public void ShouldFailAlphaNumeric()
+        {
+            Assert.Throws<ArgumentException>(delegate { Program.CalculateAreaOfCircle("number"); });
+        }
+
+        [Test]
+        public void ShouldPass1()
+        {
+            Assert.AreEqual(5881.25, Program.CalculateAreaOfCircle("43.2673"));
+        }
+    }
+}
---
+++ AreaOfACircle/AreaOfACircle/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace AreaOfACircle
+{
+    class Program
+    {
+        public static double CalculateAreaOfCircle(string radius)
+        {
+            if (Regex.IsMatch(radius, @"\d") && Convert.ToDouble(radius) > 0 && !radius.Contains(","))
+            {
+                return Math.Round(Math.PI * Math.Pow(Convert.ToDouble(radius), 2), 2);
+            }
+            throw new ArgumentException();
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CalculateAreaOfCircle("number"));
+        }
+    }
+}
