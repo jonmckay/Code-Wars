36435fcacf0dfa1e55392bb90b4118ee16e2e88a
lohkii69 <jmckay2@hotmail.com>
2015-11-09 12:36:06 -0700

    Initial commit

---
+++ LargerNumbers/LargerNumbers/LargerNumbersTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace LargerNumbers
+{
+
+    [TestFixture]
+    public class LargerNumbersTest
+    {
+        [TestCase]
+        public void getLargerNumber()
+        {
+            int[] arr1 = new int[] { 13, 64, 5, 7, 88 };
+            int[] arr2 = new int[] { 23, 4, 53, 17, 80 };
+            Assert.AreEqual(Program.getLargerNumbers(arr1, arr2), new int[] { 23, 64, 53, 17, 88 });
+        }
+    }
+}
---
+++ LargerNumbers/LargerNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LargerNumbers
+{
+    class Program
+    {
+
+        public static int[] getLargerNumbers(int[] a, int[] b)
+        {
+            var newArray = a;
+            for (var i = 0; i < a.Length; i++)
+            {
+                newArray[i] = Math.Max(a[i], b[i]);
+            }
+            return newArray;
+        }
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
