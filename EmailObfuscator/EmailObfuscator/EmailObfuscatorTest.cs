6d24828304b001e4bea373dca07ac2500fa54302
lohkii69 <jmckay2@hotmail.com>
2015-11-09 11:31:04 -0700

    Initial commit of email obfuscator. This application replaces @ with at and . with dot

---
+++ EmailObfuscator/EmailObfuscator/EmailObfuscatorTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace EmailObfuscator
+{
+    [TestFixture]
+    public class EmailObfuscatorTest
+    {
+
+        [Test]
+        public void test1()
+        {
+            Assert.AreEqual("test [at] 123 [dot] com",
+            Program.Obfuscate("test@123.com"));
+        }
+        [Test]
+        public void test2()
+        {
+            Assert.AreEqual("Code_warrior [at] foo [dot] ac [dot] uk",
+            Program.Obfuscate("Code_warrior@foo.ac.uk"));
+        }
+    }
+}
---
+++ EmailObfuscator/EmailObfuscator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace EmailObfuscator
+{
+    class Program
+    {
+        public static string Obfuscate(string email)
+        {
+            email = email.Replace("@", " [at] ");
+            return email.Replace(".", " [dot] ");
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            var email = Obfuscate("test@123.com");
+            Console.WriteLine(email);
+        }
+    }
+}
